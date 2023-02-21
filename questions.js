351564a4319811b79eaa6af73d34462683bc198d
hammmoudi <75932102+hammmoudi@users.noreply.github.com>
2023-02-21 07:31:32 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,119 @@
+questions = [
+    {
+        "question": "Freedom of business is the best practical way a society can prosper.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Charity is a better way of helping those in need than social welfare.",
+        "effect": {
+            "econ": -5,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Wages are always fair, as employers know best what a worker's labour is worth.",
+        "effect": {
+            "econ": -5,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "It is \"human nature\" to be greedy.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Communism is an ideal that can never work in practice.",
+        "effect": {
+            "econ": 10,
+            "dipl": 5,
+            "govt": -5,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Organisations and corporations cannot be trusted and need to be regulated by the government.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Land should not be a commodity to be bought and sold.",
+        "effect": {
+            "econ": -10,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Sometimes it is right that the government may spy on its citizens to combat extremists and terrorists.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "In times of crisis, safety becomes more important than civil liberties.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "If you have nothing to hide, you have nothing to fear.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 5,
+            "scty": -5
+        }
+    },
+    {
+        "question": "The government should be less involved in the day to day life of its citizens.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": -5,
+            "scty": 10
+        }
+    },
+    {
+        "question": "The smaller the government, the freer the people.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 5,
+            "scty": 10
+        }
+    },
+    {
+        "question": "The government should, at most, provide emergency services and law enforcement.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 10
+        }
+    }
+];
